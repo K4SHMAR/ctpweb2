8711b6fa731abbe9eab787d8787b4e20f6a00fe5
MrDumper <romochka-mironyuk@mail.ru>
2023-12-04 18:05:12 +0300

    C2Roma's

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world From Roma!");
         System.out.println("Hello world again!");
+        System.out.println("Hello world From Roman on the second branch!");
 
     }
 }
